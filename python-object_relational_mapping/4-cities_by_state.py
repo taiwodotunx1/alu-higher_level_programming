2b2e5d4f1dc73b025f0cf3037799f4d9f896fc24
taiwodotunx1 <a.taiwo@alustudent.com>
2026-08-01 07:59:17 -0700

    Add 4-cities_by_state file

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists states with a name starting with N (upper) from hbtn_0e_0_usa."""
+"""Lists all cities with their state from hbtn_0e_4_usa."""
 import MySQLdb
 import sys
 
@@ -17,8 +17,10 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
-                   "ORDER BY id ASC")
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
