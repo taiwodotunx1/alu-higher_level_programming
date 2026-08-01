2b5695d29e86efa05a66bfeaf382d4324f33b8df
taiwodotunx1 <a.taiwo@alustudent.com>
2026-08-01 11:55:07 -0700

    fixed model_state.py and 6-model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,14 @@
-CREATE TABLE `states` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-)
+#!/usr/bin/python3
+"""Defines the State class linked to the MySQL table states."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state, mapped to the states table."""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
