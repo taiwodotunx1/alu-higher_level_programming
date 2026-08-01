89f8a64d23cf2b51d9507da178bf4854f3583130
taiwodotunx1 <a.taiwo@alustudent.com>
2026-08-01 07:35:20 -0700

    Add 2-my_filter_states script

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists states matching a user-supplied name from hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        state_name)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
